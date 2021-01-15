6eb2047956273404b0495926490b60351a4f3636
flw <879465780@qq>
2021-01-15 22:18:44 +0800

    init spring farmework

---
+++ myreaderdemo/src/main/java/com/leven/spring/reader/Demo.java
@@ -0,0 +1,29 @@
+package com.leven.spring.reader;
+
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.Assert;
+
+public class Demo {
+//	@Test
+//	public void  test(){
+//		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+//		Person person= (Person) context.getBean("person");
+//		Assert.assertEquals("luozhou",person.getName());
+//	}
+
+	public static void main(String[] args) {
+		/*ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+		Person person= (Person) context.getBean("person");
+		//Assert.assertEquals("luozhou",person.getName());
+		System.out.println("person = " + person.getName());*/
+
+
+		XmlBeanFactory bf = new XmlBeanFactory(new ClassPathResource("bean.xml"));
+		Person person1 = (Person) bf.getBean("person");
+		System.out.println("person1.getName() = " + person1.getName());
+
+	}
+}
---
+++ myreaderdemo/src/main/java/com/leven/spring/reader/Person.java
@@ -0,0 +1,13 @@
+package com.leven.spring.reader;
+
+public class Person {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
